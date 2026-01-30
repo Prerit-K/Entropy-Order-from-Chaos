103a5961a89ab29d59322f3eab5646775b2a00b2
Prerit Kumar Ahirwar <114363820+Prerit-K@users.noreply.github.com>
2026-01-30 20:08:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "entropy-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./assets/icon.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
